df43af03f85c209aca6c126d1524d64283f46a81
Konstantin Bläsi <kblaesi@gmail.com>
2014-05-06 11:58:40 +0200

    Added readme, new ride and empty pause page. The current ride page now displays some information about the selected ride. Moved all the rides data into a service "Rides"

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,27 +1,31 @@
 angular.module('starter.controllers', [])
 
+
 .controller('AppCtrl', function($scope) {
 })
 
-.controller('NextRidesCtrl', function($scope) {
+.controller('NextRidesCtrl', function($scope, Rides) {
+	/*
   $scope.rides = [
     { start: 'Alexanderplatz, Berlin', end: 'Hauptbahnhof, Berlin', id: 1},
     { start: 'Messe, Frankfurt', end: 'Hauptbahnhof, Frankfurt', id: 2},
     { start: 'Hochschule, Offenburg', end: 'Philips, Böblingen', id: 3},
     { start: 'Flughafen, Berlin', end: 'Brandenburger Tor, Berlin', id: 4}
   ];
+  */
+  $scope.rides = Rides.all();
 })
 
-.controller('CurrentRideCtrl', function($scope, $stateParams) {
-	//var rideID = $routeParams.rideID;
+.controller('CurrentRideCtrl', function($scope, $stateParams, Rides) {
+	$scope.rideID = $stateParams.rideID;
 	//var currentRide = $scope.rides[rideID]
-	$scope.rideID = $stateParams.rideID
-	ride = 
-		{ start: 'Flughafen, Berlin', end: 'Brandenburger Tor, Berlin', id: 4 };
-
+	$scope.ride = Rides.get($stateParams.rideID);
 })
 
-.controller('NewRideCtrl', function($scope, $stateParams) {
+.controller('NewRideCtrl', function($scope, $stateParams, Rides) {
+  // initialize empty json object for rides-new view
+  $scope.ride = {};
+  //$scope.createRide = Rides.add;
 	//var rideID = $routeParams.rideID;
 	//var currentRide = $scope.rides[rideID]
 	
